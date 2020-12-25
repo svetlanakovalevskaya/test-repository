af7683b23f99a7a5e7ead1349da40e0931b0c9d5
svetlanakovalevskaya <72078444+svetlanakovalevskaya@users.noreply.github.com>
2020-12-25 21:36:28 +0200

    added test-2.js

---
+++ js/test-2.js
@@ -0,0 +1,15 @@
+const totalSpent = 2000;
+let payment = 500;
+let discount = 0;
+if (totalSpent >= 100 && totalSpent < 1000) {
+    discount = 0.02;
+    console.log(`Bronze partner! Discount ${discount * 100}%`);
+} else if (totalSpent >= 1000 && totalSpent < 5000) {
+    discount = 0.05;
+    console.log(`Silver partner! Discount ${discount * 100}%`);
+} else if (totalSpent > 5000) {
+    discount = 0.1;
+    console.log(`Gold partner! Discount ${discount * 100}%`);
+} else {
+    console.log(`Don't a partner! Discount ${discount * 100}%`);
+}
